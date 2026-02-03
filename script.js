db663d643f069ed80bb5b7cfc3490ba74d079f33
Israe <israe.yab@gmail.com>
2026-02-03 11:30:04 +0100

    fixed weather search functionality

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
-function fetchWeather() {
-    let searchInput = document.getElementById("search").value ;
-    const weatherDataSection = document.getElementById("weather-data");
-    weatherDataSection.style.display = "block";
-    const apiKey = "API_key_here"
-
-
-    if (searchInput == "") {
-  weatherDataSection.innerHTML = `
-  <div>
-    <h2>Empty Input!</h2>
-    <p>Please try again with a valid <u>city name</u>.</p>
-  </div>
-  `;
-  return;
-  
-}
-
-async function getLonAndLat() {
-  const countryCode = 212;
-  const geocodeURL =`https://api.openweathermap.org/geo/1.0/direct?q=${searchInput.replace(" ", "%20")},${countryCode}&limit=1&appid=${apiKey}`
-  const response = await fetch(geocodeURL);
-   if (!response.ok) {
-  console.log("Bad response! ", response.status);
-  return;
-}
-const data = await response.json();
-if (data.length == 0) {
-  console.log("Something went wrong here.");
-  weatherDataSection.innerHTML = `
-  <div>
-    <h2>Invalid Input: "${searchInput}"</h2>
-    <p>Please try again with a valid <u>city name</u>.</p>
-  </div>
-  `;
-  return;
-} else {
-  return data[0];
-}
-}
-async function getWeatherData(lon, lat) {
-  const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-  const response = await fetch(weatherURL);
-   if (!response.ok) {
-    console.log("Bad response! ", response.status);
+async function fetchWeather () {
+  let searchInput = document.getElementById("search").value;
+  const weatherDataSection = document.getElementById("weather-data");
+  weatherDataSection.style.display = "flex";
+  const apiKey = "1cff210b232c4f5ffecb64bce27c9e61";
+  if (searchInput === "") {
+    weatherDataSection.innerHTML = `
+    <div>
+    <h2> Empty Input !</h2>
+    <p> Please try again with a valid <u>city name </u>. </p>
+    </div>
+    `;
     return;
   }
-  const data = await response.json();
-weatherDataSection.innerHTML = `
+  async function getLonAndLat() { //function gets location's longitude coordinates from a typed name.
+  const countryCode = "MA"; //specifying the contry code to Morocco.
+  const geocodeURL = `https://api.openweathermap.org/geo/1.0/direct?q=${searchInput.replace(" ", "%20")},${countryCode}&limit=1&appid=${apiKey}`;
+  
+    const response = await fetch(geocodeURL);
+    if (!response.ok) {
+      console.log("Bad response!", response.status);
+      return;
+    }
+    const data = await response.json();
+    if (data.length == 0) {
+      console.log("Something went wrong here.");
+      weatherDataSection.innerHTML = `
+      <div>
+      <h2> Invalid Input: "${searchInput}" </h2>
+      <p> Please try again with a valid <u>city name </u>. </p>
+      </div>
+      `;
+      return;
+    } else {
+      return data [0];
+    }
+} 
+
+  async function getWeatherData(lon, lat) { //function get latitude coordinates from a typed name.
+    const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    const response = await fetch(weatherURL);
+    if (!response.ok) {
+      console.log("Bad response !", response.status);
+      return;
+    }
+    const data = await response.json();
+    weatherDataSection.innerHTML = `
 <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="${data.weather[0].description}" width="100" />
 <div>
   <h2>${data.name}</h2>
@@ -54,5 +52,13 @@ weatherDataSection.innerHTML = `
   <p><strong>Description:</strong> ${data.weather[0].description}</p>
 </div>
 `
-}
-}
+document.getElementById("search").value = "";
+  }
+
+
+
+const geocodeData = await getLonAndLat();
+getWeatherData(geocodeData.lon, geocodeData.lat);
+  }
+
+
